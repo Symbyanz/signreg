cb8efbf6653f245023001b1379b930be218ece6c
Symbyanz <pankratov.symbyanz@gmail.com>
2023-12-20 12:30:25 +0300

    test auto deploy 8

--- next.config.js
+++ next.config.js
@@ -5,10 +5,22 @@ const nextConfig = {
         config.module.rules.push({
             test: /\.svg$/i,
             issuer: /\.[jt]sx?$/,
-            use: ['@svgr/webpack'],
+            use: ['@svgr/webpack']
+        })
+        config.plugins.push({
+            apply: (compiler) => {
+                compiler.hooks.done.tap('DonePlugin', (stats) => {
+                    console.log('Compile is done !' + typeof process.env.NEXT_PUBLIC_BG_START);
+                    if(+process.env.NEXT_PUBLIC_BG_START){
+                        setTimeout(() => {
+                            process.exit(0);
+                        });
+                    }
+                });
+            }
         })
         return config
-    },
+    }
 }
 
 module.exports = nextConfig
